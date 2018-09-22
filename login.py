76ec84546752fe6e0669c690595b074a251852ef
Sai Shibu N B <saishibu38380@gmail.com>
2018-09-22 09:26:50 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,6 +1,7 @@
-import mechanize
+import mechanize,ssl
 
 theurl = 'https://192.168.1.20/login.cgi'
+ssl._create_default_https_context = ssl._create_unverified_context
 mech = mechanize.Browser()
 mech.set_handle_robots(False)
 mech.open(theurl)
