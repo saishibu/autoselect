509dffb79a7c3f3aa540afd94888a253a20a2704
Sai Shibu N B <saishibu38380@gmail.com>
2018-09-22 09:14:38 +0530

    Update nanostation.py

--- nanostation.py
+++ nanostation.py
@@ -6,7 +6,7 @@ import ssl,json,time
 import mechanize
 from bs4 import BeautifulSoup
 
-url='https://192.168.179.65/login.cgi'
+url='https://192.168.1.20/login.cgi'
 ssl._create_default_https_context = ssl._create_unverified_context
 cj=cookielib.CookieJar()
 opener=urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
@@ -18,10 +18,4 @@ print "login success"
 url2='https://192.168.179.65/link.cgi'
 br = mechanize.Browser()
 br.open(url2)
-br.set_handle_redirect(True)
-br.set_handle_robots(False)
-br.addheaders = [('User-agent', 'Firefox')]
-
-for form in br.forms():
-    print "Form name: ", form.name
-    br.select_form(nr=0) # nr=0 It selects html form without name
+print(br.title())
